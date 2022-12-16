073be1f672b22bab58a72f56f08eed5de956b4d0
LetMeEatYourLunch <sedgman.sam@gmail.com>
2022-12-17 10:50:30 +1100

    Basic pipeline skeleton

---
+++ Pipeline.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 17 10:41:42 2022
+
+@author: user
+"""
+
+class pipe_line:
+    def __init__(self, conn_yaml):
+    self.conn_yaml = conn_yaml
+    
+    def build_driving_set(self):
+    
+    def pull_dt(self, data_ds):
+        
+    def write_dt(self, data_dt):
+        
