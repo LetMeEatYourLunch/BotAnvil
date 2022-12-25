f2b9b84a435b44a7067f09eccea2d52ed10f359c
LetMeEatYourLunch <sedgman.sam@gmail.com>
2022-12-25 16:53:31 +1100

    Simple script for allowing Shiny front-end to query python back-end

---
+++ analyse_tickers.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 24 13:18:25 2022
+
+@author: user
+"""
+import os
+os.chdir("C:\\Users\\user\\Documents\\Python Scripts\\Projects\\TraderTool\\")
+import pandas as pd
+import yaml
+import requests
+import pipe_line as pl
+import numpy as np
+import collections.abc
+
+def analyse_tickers(tickers):
+    # Assertions
+    #assert(isinstance(tickers, collections.abc.Sequence))
+
+    # Create pipeline object
+    pipe = pl.pipe_line("pipeline.yml")
+    # Define empty data frame
+    df_all = pd.DataFrame(columns=["code"
+                                   , "close_date"
+                                   , "close_price"
+                                   , "change_price"
+                                   , "volume"
+                                   , "day_high_price"
+                                   , "day_low_price"
+                                   , "change_in_percent"
+                                   , "delta"
+                                   , "loss"
+                                   , "gain"
+                                   , "loss_mean"
+                                   , "gain_mean"
+                                   , "RSI"
+                                   , "ema"
+                                   , "ema_12"
+                                   , "ema_26"
+                                   , "macd"])
+    for ticker in tickers:
+        df_ticker = pipe.pull_dt(ticker)
+        df_ticker = pipe.get_rsi(df_ticker)
+        df_ticker = pipe.get_macd(df_ticker)
+        df_all = pd.concat([df_all, df_ticker])
+        
+    
+    return df_all
+    
+        
+    
